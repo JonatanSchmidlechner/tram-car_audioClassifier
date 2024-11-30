66be78dd44b24200ab89c8779613daa243c30a42
Arto Korpiharju <arto.korpiharju@tuni.fi>
2024-11-30 18:11:24 +0200

    audio samples and loading/normalizing functions to audioClassifier

--- audioClassifier.py
+++ audioClassifier.py
@@ -1,19 +1,90 @@
+import os
 import librosa
 import numpy as np
+import sounddevice as sd
+import scipy.signal as signal
 
-SAMPLINGRATE: int = 16000
+def load_all_audio(folder_path: str):
 
-def loadAllAudio(fileName: str):
-    audios: list = []
-    # TODO: Get labels
-    labels: list = []
-    # TODO: Add loop to load all files.
-    audio: np.ndarray
-    samplingFreq: int
-    audio, samplingFreq = librosa.load(fileName, sr=SAMPLINGRATE) # This automatically scales to 16kHz
+    # List to store the audio data
+    audios = []
 
-    audios.append(audio)
-    return audios, labels
+    # Loop through the audio files in the given folder
+    for file_name in os.listdir(folder_path):
+
+        # Path to the audio file
+        file_path = os.path.join(folder_path, file_name)
+        print(f"Loading: {file_path}")
+
+        # Try to load the file, error just in case
+        try:
+            # Load the audio file
+            audio, sr = librosa.load(file_path, sr=16000, mono=True)
+
+            # Append the data as a dictionary
+            audios.append({
+                "fileName": file_name,
+                "audio": audio
+            })
+        except Exception as e:
+            print(f"Error loading {file_name}: {e}")
+
+    return audios
+
+
+def normalize_audio(audio):
+
+    # Frequency normalization. Removing irrelevant frequencies using bandpass filter.
+    # Input the frequency range you want.
+
+    # values must be 0 < x < 8000. Might want to mess with these when testing learning.
+    audio = normalize_frequency(audio, 10, 7999)
+
+
+    # Logarithmically scale audio
+
+    # might be helpful or not
+    #logarithmic_scaling(audio)
+
+
+    # Amplitude normalization by Peak Normalization. Normalized -1 to 1.
+    audio = normalize_amplitude(audio)
+
+    return audio
+
+
+def normalize_amplitude(audio):
+
+    # Take peak from every audio file and divide file by it.
+    for audio_data in audio:
+        peak = np.max(np.abs(audio_data["audio"]))
+
+        audio_data["audio"] = audio_data["audio"] / peak
+
+    return audio
+
+
+def normalize_frequency(audio, lowcut, highcut, sr=16000):
+
+    # Normalize frequency range to nyquist
+    nyquist = sr / 2
+    low = lowcut / nyquist
+    high = highcut / nyquist
+
+    # Remove unwanted frequencies with a bandpass filter
+    for audio_data in audio:
+        sos = signal.butter(10, [low, high], btype='band', output='sos')
+        audio_data["audio"] = signal.sosfilt(sos, audio_data["audio"])
+
+    return audio
+
+def logarithmic_scaling(audio, epsilon=1e-16):
+
+    # Logarithmically scale audio. Add Epsilon in case of 0s.
+    for audio_data in audio:
+        audio_data["audio"] = np.sign(audio_data["audio"]) * np.log(np.abs(audio_data["audio"]) + epsilon)
+
+    return audio
 
 def extract_features(audio):
     mfccs: np.ndarray = librosa.feature.mfcc(y=audio, sr=SAMPLINGRATE, n_mfcc=13)
@@ -29,10 +100,17 @@ def extract_features(audio):
 
 
 def main():
-    # TODO: convert audio to wav?
-    # TODO: normalize amplitude and maybe some other stuff: duration?
-    audio, labels = loadAllAudio('Auto1.m4a', 16000)
-    features = extract_features(audio[0])
+    # Load audio data
+    # For the sake of efficiency audio has been normalized to 16kHz mono sound in advance.
+    # This way it does not need to be done every time the program is tested and takes a lot less disk space.
+    car_audio = load_all_audio("carSamples")
+    tram_audio = load_all_audio("tramSamples")
+
+    # Normalize audio signals. For more information checkout the normalize_audio function.
+    car_audio = normalize_audio(car_audio)
+    tram_audio = normalize_audio(tram_audio)
+
+    #features = extract_features(audio[0])
     # TODO: Split data to train and test. Use random shuffle? Also then shuffle labels accordingly.
     # TODO: Train model
 
