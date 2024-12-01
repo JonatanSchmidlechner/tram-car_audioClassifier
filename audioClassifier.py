b093e53d632bc06a91d957e223cc03c36a022c4c
Jonatan <jonatan.schmidlechner@tuni.fi>
2024-12-01 19:47:02 +0200

    Add sample duration normalization, add classifier, add feature extraction, refactor code.

--- audioClassifier.py
+++ audioClassifier.py
@@ -1,129 +1,197 @@
 import os
 import librosa
 import numpy as np
-import sounddevice as sd
 import scipy.signal as signal
 import soundfile as sf
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
-def load_all_audio(folder_path: str):
+NORMALIZED_SAMPLINGRATE: int = 16000 # Hz
+FIXED_DURATION: float = 5.0 # Seconds
 
+def load_all_audio(folderPath: str, label: int) -> tuple[np.ndarray, np.ndarray]:
     # List to store the audio data
-    audios = []
-
+    audios: list = []
+    labels: list = []
     # Loop through the audio files in the given folder
-    for file_name in os.listdir(folder_path):
-
+    fileName: str
+    for fileName in os.listdir(folderPath):
         # Path to the audio file
-        file_path = os.path.join(folder_path, file_name)
-        print(f"Loading: {file_path}")
+        filePath: str = os.path.join(folderPath, fileName)
 
         # Try to load the file, error just in case
         try:
             # Load the audio file
-            audio, sr = sf.read(file_path)
+            audio: np.ndarray
+            sr: int
+            audio, sr = sf.read(filePath)
 
             # Resample if sampling rate is not 16000 as it should already be.
-            if sr != 16000:
-                num_samples = int(len(audio) * 16000 / sr)
-                audio = signal.resample(audio, num_samples)
-                sr = 16000
+            if sr != NORMALIZED_SAMPLINGRATE:
+                numSamples: int = int(len(audio) * NORMALIZED_SAMPLINGRATE / sr)
+                audio = signal.resample(audio, numSamples)
+                sr = NORMALIZED_SAMPLINGRATE
 
             # Append the data as a dictionary
             audios.append({
-                "fileName": file_name,
+                "fileName": fileName,
                 "audio": audio
             })
+            labels.append(label)
         except Exception as e:
-            print(f"Error loading {file_name}: {e}")
+            print(f"Error loading {fileName}: {e}")
 
-    return audios
+    return audios, labels
 
 
-def normalize_audio(audio):
+def normalize_audio(audio: np.ndarray) -> np.ndarray:
 
     # Frequency normalization. Removing irrelevant frequencies using bandpass filter.
     # Input the frequency range you want.
 
     # values must be 0 < x < 8000. Might want to mess with these when testing learning.
     audio = normalize_frequency(audio, 10, 7999)
-
+    # Samples need to have a fixed duration for ML classifier model.
+    audio = normalize_duration(audio, FIXED_DURATION, NORMALIZED_SAMPLINGRATE)
 
     # Logarithmically scale audio
 
     # might be helpful or not
     #logarithmic_scaling(audio)
 
-
     # Amplitude normalization by Peak Normalization. Normalized -1 to 1.
     audio = normalize_amplitude(audio)
-
     return audio
 
 
-def normalize_amplitude(audio):
+def normalize_amplitude(audio: np.ndarray) -> np.ndarray:
 
     # Take peak from every audio file and divide file by it.
-    for audio_data in audio:
-        peak = np.max(np.abs(audio_data["audio"]))
+    audioData: dict
+    for audioData in audio:
+        peak: int = np.max(np.abs(audioData["audio"]))
 
-        audio_data["audio"] = audio_data["audio"] / peak
+        audioData["audio"] = audioData["audio"] / peak
 
     return audio
 
 
-def normalize_frequency(audio, lowcut, highcut, sr=16000):
+def normalize_frequency(audio: np.ndarray, lowcut: int, highcut: int,
+                         sr: int=NORMALIZED_SAMPLINGRATE) -> np.ndarray:
 
     # Normalize frequency range to nyquist
-    nyquist = sr / 2
-    low = lowcut / nyquist
-    high = highcut / nyquist
+    nyquist: int = sr / 2
+    low: int = lowcut / nyquist
+    high: int = highcut / nyquist
 
     # Remove unwanted frequencies with a bandpass filter
-    for audio_data in audio:
-        sos = signal.butter(10, [low, high], btype='band', output='sos')
-        audio_data["audio"] = signal.sosfilt(sos, audio_data["audio"])
+    audioData: dict
+    for audioData in audio:
+        sos: np.ndarray = signal.butter(10, [low, high], btype='band', output='sos')
+        audioData["audio"] = signal.sosfilt(sos, audioData["audio"])
 
     return audio
 
-def logarithmic_scaling(audio, epsilon=1e-16):
+def normalize_duration(audio: np.ndarray, fixedDuration: float=FIXED_DURATION,
+                        samplingRate: int=NORMALIZED_SAMPLINGRATE) -> np.ndarray:
+    audioData: dict
+    for audioData in audio:
+        numSamples: int = int(fixedDuration * samplingRate)
+        audioLength: int = len(audioData["audio"])
+        if audioLength > numSamples:
+            audioData["audio"] = audioData["audio"][:numSamples]
+        else:
+            padding = np.zeros(numSamples - len(audioData["audio"]))
+            audioData["audio"] = np.concatenate([audioData["audio"], padding])
+    return audio
+
+def logarithmic_scaling(audio: np.ndarray, epsilon: float=1e-16) -> np.ndarray:
 
     # Logarithmically scale audio. Add Epsilon in case of 0s.
-    for audio_data in audio:
-        audio_data["audio"] = np.sign(audio_data["audio"]) * np.log(np.abs(audio_data["audio"]) + epsilon)
+    audioData: dict
+    for audioData in audio:
+        audioData["audio"] = np.sign(audioData["audio"]) * np.log(np.abs(audioData["audio"]) + epsilon)
 
     return audio
 
-def extract_features(audio):
-    mfccs: np.ndarray = librosa.feature.mfcc(y=audio, sr=SAMPLINGRATE, n_mfcc=13)
-    delta: np.ndarray = librosa.feature.delta(mfccs)
-    delta2: np.ndarray = librosa.feature.delta(mfccs, order=2)
+def extract_features(audio: np.ndarray) -> np.ndarray:
+    allFeatures: list = []
+    audioData: dict
+    for audioData in audio:
+        audio = audioData["audio"]
+        # Extract features
+        mfccs: np.ndarray = librosa.feature.mfcc(y=audio, sr=NORMALIZED_SAMPLINGRATE, n_mfcc=13)
+        delta: np.ndarray = librosa.feature.delta(mfccs)
+        delta2: np.ndarray = librosa.feature.delta(mfccs, order=2)
+        zcr: np.ndarray = librosa.feature.zero_crossing_rate(audio)
+        # Should we take the mean of the features to lower the dimensionality?
+        mfccsMean: np.ndarray = np.mean(mfccs.T, axis=0)
+        deltaMean: np.ndarray = np.mean(delta.T, axis=0)
+        delta2Mean: np.ndarray = np.mean(delta2.T, axis=0)
+        zcrMean: np.ndarray = np.mean(zcr.T, axis=0)
+        # Add concatenated features to the list of all features
+        allFeatures.append(np.hstack([mfccsMean.T, deltaMean.T, delta2Mean.T, zcrMean.T]).flatten())
 
-    # Should we take the mean of the features to lower the dimensionality?
-    mfccsMean: np.ndarray = np.mean(mfccs.T, axis=0)
-    deltaMean: np.ndarray = np.mean(delta.T, axis=0)
-    delta2Mean: np.ndarray = np.mean(delta2.T, axis=0)
-    # TODO: Add 4th feature
-    return [mfccsMean, deltaMean, delta2Mean]
-
+    sc: StandardScaler = StandardScaler()
+    scaledFeatures: np.ndarray = sc.fit_transform(allFeatures)
+    return np.array(scaledFeatures)
 
 def main():
     # Load audio data
     # For the sake of efficiency audio has been normalized to 16kHz mono sound in advance.
     # This way it does not need to be done every time the program is tested and takes a lot less disk space.
-    car_audio = load_all_audio("carSamples")
-    tram_audio = load_all_audio("tramSamples")
+    carAudio: np.ndarray
+    carLabels: np.ndarray
+    carAudio, carLabels = load_all_audio("carSamples", 0)
 
-    # Normalize audio signals. For more information checkout the normalize_audio function.
-    car_audio = normalize_audio(car_audio)
-    tram_audio = normalize_audio(tram_audio)
+    tramAudio: np.ndarray
+    tram_labels: np.ndarray
+    tramAudio, tram_labels = load_all_audio("tramSamples", 1)
 
-    #features = extract_features(audio[0])
-    # TODO: Split data to train and test. Use random shuffle? Also then shuffle labels accordingly.
-    # TODO: Train model
+    # Normalize audio signals.
+    carAudio: np.ndarray = normalize_audio(carAudio)
+    tramAudio: np.ndarray = normalize_audio(tramAudio)
+    audioSamples: np.ndarray = np.concatenate((carAudio, tramAudio), axis=0)
+    audioLabels: np.ndarray = np.concatenate((carLabels, tram_labels), axis=0)
+    # Extract features from audio samples.
+    features: np.ndarray = extract_features(audioSamples)
 
+    # Split data to train, validation and test sets.
+    xTrain: np.ndarray
+    yTrain: np.ndarray
+    xValTest: np.ndarray
+    yValTest: np.ndarray
+    xVal: np.ndarray
+    yVal: np.ndarray
+    xTest: np.ndarray
+    yTest: np.ndarray
+    # Extract train data.
+    xTrain, xValTest, yTrain, yValTest = train_test_split(features, audioLabels, test_size=0.4, random_state=42)
+    # Extract validation and test data. Validation data is used for tuning the model.
+    xVal, xTest, yVal, yTest = train_test_split(xValTest, yValTest, test_size=0.5, random_state=42)
 
+    # Train the model
+    clf: RandomForestClassifier = RandomForestClassifier(n_estimators=100)
+    clf.fit(xTrain, yTrain)
+    # Test the model
+    yValPred: np.ndarray = clf.predict(xVal)
+    accuracy: float = accuracy_score(yVal, yValPred)
+    print(f"Accuracy: {accuracy}")
+    print(classification_report(yVal, yValPred))
 
-
+    # Sort and plot feature importances
+    featureImportances: np.ndarray = clf.feature_importances_
+    sortedIndices: np.ndarray = np.argsort(featureImportances)[::-1]
+    plt.figure(figsize=(10, 6))
+    plt.title("Feature Importances")
+    plt.bar(range(len(featureImportances)), featureImportances[sortedIndices], align="center")
+    plt.xticks(range(len(featureImportances)), sortedIndices)
+    plt.xlabel("Feature Index")
+    plt.ylabel("Importance")
+    plt.show()
 
 if __name__ == '__main__':
     main()
